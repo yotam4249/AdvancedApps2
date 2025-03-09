4136f4afbcdc2be4f0b46b7ea6fc1294b965253f
EliavC <eliavc2001@gmail.com>
2025-03-09 15:15:17 +0200

    cross origin

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,12 @@ const options = {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","*");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    next();
+})
 app.use("/posts",postRoutes);
 app.use("/comments",commentsRoutes);
 app.use("/auth",authRoutes)
