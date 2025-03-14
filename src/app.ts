01323c0030cc3687d6ab6d7e3f88a557feb1b5bd
EliavC <eliavc2001@gmail.com>
2025-03-14 18:04:38 +0200

    p

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 
 initApp().then((app) =>{
     app.listen(port,()=>{
-        console.log(`Listenenig on port : http://127.0.0.1:${port}`);
+        console.log(`Listenenig on port : http://localhost:${port}`);
     });
 })
     
