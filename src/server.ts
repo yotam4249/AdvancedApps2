4fdc1cc4b42b92bf51d7333fef4e48b0d392e356
yotam4249 <136727458+yotam4249@users.noreply.github.com>
2025-03-19 21:54:06 +0200

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -42,12 +42,15 @@ app.use(
     cors({
       origin: "http://localhost:5173", 
       credentials: true, 
-      methods: "GET,POST,PUT,DELETE", 
+      methods: "GET,POST,PUT,DELETE,PATCH", 
       allowedHeaders: "Content-Type,Authorization", 
     })
   );
   app.use(cookieParser());
 const storagePath = "/Users/yotammizrahi/Desktop/Projects/AdvancedApps2/storage"; 
+//const storagePath = "C:\Users\eliav\myapp1\Task2EY\AdvancedApps2\storage";
+
+
 
 //  app.use(cors({
 //      origin: "http://localhost:5173", // Allow frontend access
